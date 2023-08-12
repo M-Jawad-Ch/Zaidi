fd3776116563daa4579f844d773aba32ab1d56c9
jawad <mjawadch55@gmail.com>
2023-08-12 14:31:01 +0500

    Fixed Contact us

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.index, name='main'),
     path("favicon.ico/", views.return_404),
     path("contact/", views.add_contact),
+    path('contact-us/', views.contact),
     path('<str:extra_page>/', views.extra_page),
     path("categories/<str:slug>/", views.get_category),
     re_path(r"(?P<category>^(?!image).*)/(?P<post>.*)/comment", views.comment),
