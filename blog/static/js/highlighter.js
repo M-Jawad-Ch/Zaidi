6a46485a9f2c45fecbb3b58d824658df32337cb3
jawad <mjawadch55@gmail.com>
2023-08-18 00:55:24 +0500

    Added code indentation

--- blog/static/js/highlighter.js
+++ blog/static/js/highlighter.js
@@ -18,29 +18,80 @@ for (const textArea of textAreas) {
 form.addEventListener('submit', (e) => {
     for (const textArea of textAreas) {
         let div = textArea.parentElement.querySelector('div.textarea')
+        div.innerHTML = div.innerHTML.replace(/&nbsp;/g, '')
         textArea.value = div.innerText
     }
 })
 
+function format(html) {
+    var tab = '\t\t\t\t';
+    var result = '';
+    var indent = '';
+
+    html.split(/>\s*</).forEach(function (element) {
+        if (element.match(/^\/\w/)) {
+            indent = indent.substring(tab.length);
+        }
+
+        result += indent + '<' + element + '>\r\n';
+
+        if (element.match(/^<?\w[^>]*[^\/]$/) && !element.startsWith("input")) {
+            indent += tab;
+        }
+    });
+
+    return result.substring(1, result.length - 3);
+}
+
 var divs = document.querySelectorAll('div.textarea')
 
 for (const div of divs) {
-    let text = div.innerText
+    let text = format(div.innerText)
 
-    let arr = text.split('.').filter(val => val ? true : false).map(x => x.trim())
+    let arr = text.split(new RegExp(/\.[\s\n]/g)).filter(val => val ? true : false)
+    let trimmed_arr = arr.map(x => x.trim().replace(new RegExp(/[\s\.]/g), ''))
 
     div.innerHTML = ''
-    arr.forEach((value, index, arr) => {
-        if (arr.indexOf(value) != index) {
-            let element = document.createElement('span')
-            element.classList.add('highlight')
-            element.innerText = value + '.'
+    trimmed_arr.forEach((value, index, array) => {
+        let element = null, text = arr[index]
 
-            div.appendChild(element)
+        if (array.indexOf(value) != index) {
+            element = document.createElement('span')
+            element.classList.add('highlight')
         }
-        else {
-            let element = document.createTextNode(value + '.')
-            div.appendChild(element)
+
+        if (index < array.length - 1) {
+            text += '. '
         }
+
+        if (element) {
+            element.innerText = text
+        }
+
+        element = element ?? document.createTextNode(text)
+
+        div.appendChild(element)
     })
+}
+
+for (const div of divs) {
+    let text = div.innerHTML
+
+    let arr = text.split('\n')
+
+    div.innerHTML = ''
+    arr.forEach((value, index, array) => {
+        let parent = document.createElement('span')
+        parent.innerHTML = value
+
+        if (index < array.length - 1) {
+            parent.appendChild(document.createElement('br'))
+        }
+
+        div.appendChild(parent)
+    })
+}
+
+for (const div of divs) {
+    div.innerHTML = div.innerHTML.replace(/[\t]/g, '&nbsp;')
 }
